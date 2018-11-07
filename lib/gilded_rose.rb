1a1f8aff0a52ddbe315ad9018b6abdcc920a4992
Al <>
2018-11-07 11:15:26 +0000

    reordered conditions to group outcomes by name and apply a single change to quality based on sell_in

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,40 +7,42 @@ class GildedRose
   def update_quality
     @items.each do |item|
       break if item.name == "Sulfuras, Hand of Ragnaros"
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        decrement_quality(item)
-      else
-        increment_quality(item)
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          increment_quality(item) if item.sell_in < 11
-          increment_quality(item) if item.sell_in < 6
-        end
-      end
       item.sell_in -= 1
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            decrement_quality(item)
-          else
-            item.quality = 0
-          end
+
+      case item.name
+      when "Aged Brie"
+        if item.sell_in < 0
+          change_quality(item, 2)
         else
-          increment_quality(item)
+          change_quality(item, 1)
+        end
+      when "Backstage passes to a TAFKAL80ETC concert"
+        if item.sell_in < 0
+          item.quality = 0
+        elsif item.sell_in < 6
+          change_quality(item, 3)
+        elsif item.sell_in < 11
+          change_quality(item, 2)
+        else
+          change_quality(item, 1)
+        end
+      else
+        if item.sell_in < 0
+          change_quality(item, -2)
+        else
+          change_quality(item, -1)
         end
       end
+
       limit_quality(item)
     end
   end
 
-  def increment_quality(item)
-    item.quality += 1
-  end
-
-  def decrement_quality(item)
-    item.quality -= 1
+  def change_quality(item, multiplier)
+    item.quality += multiplier
   end
 
   def limit_quality(item)
-    item.quality = item.quality.clamp(0,50)
+    item.quality = item.quality.clamp(0, 50)
   end
 end
